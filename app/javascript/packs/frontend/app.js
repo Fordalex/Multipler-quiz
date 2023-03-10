5cd23ebd401681429bb55c4ee14c7d2fb6cfd721
Fordalex <alex96ford19@gmail.com>
2023-03-10 20:57:32 +0000

    badly add vuejs.

---
+++ app/javascript/packs/frontend/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {}
+  },
+  methods: {}
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  app.mount("#app");
+})
+
+export default app
---
+++ app/javascript/packs/frontend/components/PlayerLarge.js
@@ -0,0 +1,11 @@
+import app from '../app.js'
+
+app.component('player', {
+  template:
+/*html*/`
+<div>
+  <p>asdf</p>
+</div>
+`
+})
+
