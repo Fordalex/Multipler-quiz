d9f3ccb0a7128c6dd27c1ff656e6e52ac720c3c3
Fordalex <alex96ford19@gmail.com>
2023-03-11 16:47:34 +0000

    add jest for testing

---
+++ app/javascript/tests/Player.spec.js
@@ -0,0 +1,8 @@
+// import Player from '../packs/components/Player.vue';
+// import { mount } from '@vue/test-utils';
+
+describe('Player', () => {
+  test('is a Vue instance', () => {
+    expect(true).toBe(true)
+  })
+});
