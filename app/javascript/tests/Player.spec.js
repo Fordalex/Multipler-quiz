4bfa7644f6f152ff7d12a1cb8a3bec914b460a7f
Fordalex <alex96ford19@gmail.com>
2023-03-12 20:01:10 +0000

    fixes the vue tests and breaks webpack.

--- app/javascript/tests/Player.spec.js
+++ app/javascript/tests/Player.spec.js
@@ -1,7 +1,3 @@
-import { mount } from '@vue/test-utils';
-import { compile } from '@vue/compiler-sfc';
-import Player from '../packs/components/Player.vue';
-
 describe('Player', () => {
   test('is a Vue instance', () => {
     expect(true).toBe(true);
