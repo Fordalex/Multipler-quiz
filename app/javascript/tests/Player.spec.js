a1fc3864833cc475a620198046c7e450b99faa7a
Fordalex <alex96ford19@gmail.com>
2023-03-12 17:06:41 +0000

    update vue testing stuff

--- app/javascript/tests/Player.spec.js
+++ app/javascript/tests/Player.spec.js
@@ -1,9 +1,9 @@
-// import Player from '../packs/components/Player.vue';
 import { mount } from '@vue/test-utils';
-
+import { compile } from '@vue/compiler-sfc';
+import Player from '../packs/components/Player.vue';
 
 describe('Player', () => {
   test('is a Vue instance', () => {
-    expect(true).toBe(true)
-  })
+    expect(true).toBe(true);
+  });
 });
