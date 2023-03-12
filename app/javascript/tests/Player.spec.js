52e7c08ae92555df5331baa55c196b9db9b772cc
Fordalex <alex96ford19@gmail.com>
2023-03-12 13:36:44 +0000

    update config for jest

--- app/javascript/tests/Player.spec.js
+++ app/javascript/tests/Player.spec.js
@@ -1,5 +1,6 @@
 // import Player from '../packs/components/Player.vue';
-// import { mount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+
 
 describe('Player', () => {
   test('is a Vue instance', () => {
