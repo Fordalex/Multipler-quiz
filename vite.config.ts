6685f9587bed4dddc84a2f357c84b99cfd055b2e
Fordalex <alex96ford19@gmail.com>
2023-03-18 13:50:59 +0000

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 import vue from '@vitejs/plugin-vue'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig({
   plugins: [
     vue(),
     RubyPlugin(),
     FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 }),
-  ]
+  ],
+  server: {
+    host: true,
+  },
 })
